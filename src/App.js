e54242c03a23528f0f6638ebacb08f68edb83734
unknown <ankit.bisht@tothenew.com>
2022-04-03 19:14:50 +0530

    post.jsx added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import './css/style.css'
 import Login from "./components/login.jsx"
+import Post from "./components/post.jsx"
 
 
 function App() {
   return (
     <div className="App">
-    <Login />
+    <Post />
     </div>
   );
 }
