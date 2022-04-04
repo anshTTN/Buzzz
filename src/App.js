02512c745cc35e916d438ab2d8b4be80c85b01f8
unknown <ankit.bisht@tothenew.com>
2022-04-04 14:33:24 +0530

    activity.jsx added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
 import './App.css';
-import './css/style.css'
-import Login from "./components/login.jsx"
-import Post from "./components/post.jsx"
+import './css/style.css';
+import Login from "./components/login.jsx";
+import Post from "./components/post.jsx";
+import Activity from "./components/activity.jsx";
+import Suggestions from "./components/suggestions.jsx";
 
 
 function App() {
   return (
     <div className="App">
     <Post />
+
+    <Suggestions />
     </div>
   );
 }
